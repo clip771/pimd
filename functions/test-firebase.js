b74b436765f194ed0c7dd7280b657f0789501b6a
clip777 <mauricio.sk8.dias@gmail.com>
2025-07-03 18:57:18 -0300

    Atualiza arquivos e netlify.toml

---
+++ functions/test-firebase.js
@@ -0,0 +1,26 @@
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+    }),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+  });
+}
+
+exports.handler = async function (event, context) {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, message: "Conexão Firebase OK" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false, error: error.message }),
+    };
+  }
+};
