cb119596ce2cc951eec8e47a3301a83a239d2b82
clip777 <mauricio.sk8.dias@gmail.com>
2025-07-03 17:51:27 -0300

    Atualiza arquivos e netlify.toml

--- functions/testar-env.js
+++ functions/testar-env.js
@@ -1,12 +1,10 @@
-// /functions/testar-env.js
-
 exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY ? "OK" : "NÃO DEFINIDO",
-      FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL ? "OK" : "NÃO DEFINIDO",
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID ? "OK" : "NÃO DEFINIDO",
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || null,
+      FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL || null,
+      FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY ? "OK" : null
     }),
   };
 };
