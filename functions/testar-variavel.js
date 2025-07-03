6c3d4c411dcfa91d7081701a1be902e75a4c27e0
clip777 <mauricio.sk8.dias@gmail.com>
2025-07-03 16:47:59 -0300

    Atualiza arquivos e netlify.toml

---
+++ functions/testar-variavel.js
@@ -0,0 +1,23 @@
+export const handler = async () => {
+  const jsonString = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+  console.log("========== RAW STRING ==========");
+  console.log(jsonString);
+
+  const serviceAccount = JSON.parse(jsonString);
+
+  console.log("========== PRIVATE KEY ==========");
+  console.log(serviceAccount.private_key);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      private_key_length: serviceAccount.private_key.length,
+      starts_with: serviceAccount.private_key.slice(0, 30),
+      ends_with: serviceAccount.private_key.slice(-30)
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+};
